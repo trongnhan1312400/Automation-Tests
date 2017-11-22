a73ad50e6da2b1e35c035ec74bec57387db959d2
Khoi Ngo <khoi.ngo@logigear.com>
2017-11-22 17:00:24 +0700

    add testcase 02,04

--- test_scripts/acceptance_tests/keyrings_wallets.py
+++ test_scripts/acceptance_tests/keyrings_wallets.py
@@ -8,37 +8,43 @@ import json
 import os.path
 import sys
 from indy import signus
+from indy.error import IndyError
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from libraries.constant import Constant, Colors, Roles
+from libraries.result import Status
+from libraries.common import Common
+from libraries.utils import *
 from test_scripts.test_scenario_base import TestScenarioBase
 
 
 class TestScenario04(TestScenarioBase):
 
     def __init__(self):
-        super(TestScenarioBase, self).__init__(3, "test_scenario_04_keyrings_wallet")
+        super().__init__(total_steps=3, test_name="test_scenario_04_keyrings_wallet")
 
     async def execute_test_case(self):
         print("Test Scenario 04 -> started")
         seed_default_trustee = "000000000000000000000000Trustee1"
         try:
             # 1. Create and open pool Ledger  ---------------------------------------------------------
-            self.steps[1].set_name("Create and open pool Ledger")
-            returned_code = await perform(self.steps[1], Common.prepare_pool_and_wallet, self.pool_name,
-                                   self.wallet_name, self.pool_genesis_txn_file)
-    
+            self.steps[0].set_name("Create and open pool Ledger")
+            returned_code = await perform(self.steps[0], Common.prepare_pool_and_wallet, self.pool_name,
+                                          self.wallet_name, self.pool_genesis_txn_file)
+
             self.pool_handle, self.wallet_handle = raise_if_exception(returned_code)
-    
+
             # 2. verify wallet was created in .indy/wallet
-            self.steps[2].set_name("Verify wallet was created in .indy/wallet")
-            wallet_path = Constant.work_dir + "/wallet/" + wallet_name
+            self.steps[1].set_name("Verify wallet was created in .indy/wallet")
+            wallet_path = Constant.work_dir + "/wallet/" + self.wallet_name
             result = os.path.exists(wallet_path)
             if result:
-                self.steps[2].set_status(Status.PASSED)
-    
+                self.steps[1].set_status(Status.PASSED)
+
             # 3. create DID to check the new wallet work well.
-            self.steps[3].set_name("Create DID to check the new wallet work well")
-            await perform(self.steps[3], signus.create_and_store_my_did,
-                          wallet_handle, json.dumps({"seed": seed_default_trustee}))
+            self.steps[2].set_name("Create DID to check the new wallet work well")
+            await perform(self.steps[2], signus.create_and_store_my_did,
+                          self.wallet_handle, json.dumps({"seed": seed_default_trustee}))
         except IndyError as e:
             print(Colors.FAIL + "Stop due to IndyError: " + str(e) + Colors.ENDC)
         except Exception as ex:
@@ -48,5 +54,4 @@ class TestScenario04(TestScenarioBase):
 
 
 if __name__ == '__main__':
-    test_scenario = TestScenario04()
-    test_scenario.execute_scenario()
+    TestScenario04().execute_scenario()
--- test_scripts/acceptance_tests/verify_messages_on_connection.py
+++ test_scripts/acceptance_tests/verify_messages_on_connection.py
@@ -1,103 +1,68 @@
 import json
-import logging
-import os
+import os.path
 import sys
-import time
 from indy import pool
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
-from libraries.constant import Constant, Colors
-from libraries.utils import *
-from libraries.result import TestResult, Status
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from libraries.constant import Constant, Colors, Roles
+from libraries.result import Status
 from libraries.common import Common
+from libraries.utils import *
+from test_scripts.test_scenario_base import TestScenarioBase
+
+""" Global variables. """
+pool_genesis_txn_file = Constant.pool_genesis_txn_file
+original_pool_genesis_txn_file = Constant.original_pool_genesis_txn_file
+the_error_message = "the information needed to connect was not found"
+
+""" cmds """
+back_up_pool_genesis_file = 'cp ' + pool_genesis_txn_file + " " + original_pool_genesis_txn_file
+exit_sovrin = 'exit'
+remove_pool_genesis_file = 'rm ' + pool_genesis_txn_file
+restore_pool_genesis_file = 'cp ' + original_pool_genesis_txn_file + " " + pool_genesis_txn_file
 
 
-# -----------------------------------------------------------------------------------------
-# This will run acceptance tests that will validate the add/remove roles functionality.
-# -----------------------------------------------------------------------------------------
+class TestScenario02(TestScenarioBase):
 
-class Variables:
-    the_error_message = "the information needed to connect was not found"
-    test_result = TestResult("Test_Scenario_02_Verify_Messages_On_Connection")
-    pool_name = generate_random_string("test_pool")
-    wallet_name = generate_random_string("test_wallet")
-    debug = False
-    steps = create_step(5)
+    def __init__(self):
+        super().__init__(total_steps=3, test_name="test_scenario_02_verify_messages_on_connection")
 
-    """  Needed some global variables. """
-    pool_handle = 0
-    pool_genesis_txn_file = Constant.pool_genesis_txn_file
-    original_pool_genesis_txn_file = Constant.original_pool_genesis_txn_file
-    pool_name = generate_random_string("test_pool", size=20)
-    debug = False
+    def run(self, cmd):
+        os.system(cmd)
 
-    # cmds
-    back_up_pool_genesis_file = 'cp ' + pool_genesis_txn_file + " " + original_pool_genesis_txn_file
-    exit_sovrin = 'exit'
-    remove_pool_genesis_file = 'rm ' + pool_genesis_txn_file
-    restore_pool_genesis_file = 'cp ' + original_pool_genesis_txn_file + " " + pool_genesis_txn_file
+    def execute_precondition_steps(self):
+        self.run(back_up_pool_genesis_file)
+        open(pool_genesis_txn_file, 'w').close()
 
+    async def execute_postcondition_steps(self):
+        self.run(remove_pool_genesis_file)
+        self.run(restore_pool_genesis_file)
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
-
-
-def run(cmd):
-    os.system(cmd)
-
-
-def test_precondition():
-    """  Make a copy of pool_transactions_sandbox_genesis  """
-    Variables.steps[0].set_name("Precondition")
-    run(Variables.back_up_pool_genesis_file)
-    open(Variables.pool_genesis_txn_file, 'w').close()
-    Variables.steps[0].set_status(Status.PASSED)
-
-
-async def test_scenario_02_verify_messages_on_connection():
-    logger.info("Test Scenario 02 -> started")
-
-    try:
-        # 1. Create ledger config from genesis txn file  ---------------------------------------------------------
-        Variables.steps[1].set_name("Create Ledger")
-        pool_config = json.dumps({"genesis_txn": str(Variables.pool_genesis_txn_file)})
-        Variables.pool_handle = await perform(Variables.steps[1], pool.create_pool_ledger_config, Variables.pool_name, pool_config)
-
-        # 2. Open pool ledger -----------------------------------------------------------------------------------
-        Variables.steps[2].set_name("Open pool ledger")
-        Variables.steps[2].set_message("Failed due to the Bug IS-332")
-        Variables.steps[2].set_status(Status.FAILED)
-
-        # 3. verifying the message ------------------------------------------------------------------------
-        Variables.steps[3].set_name("verifying the message")
-        Variables.steps[3].set_message("TODO after fix IS-332")
-        Variables.steps[3].set_status(Status.FAILED)
-
-    finally:
-        # 4. Restore the pool_transactions_sandbox_genesis file-------------------------------------------------
-        Variables.steps[4].set_name("Restore the pool_transactions_sandbox_genesis file")
+    async def execute_test_case(self):
+        print("Test Scenario 02 -> started")
         try:
-            run(Variables.remove_pool_genesis_file)
-            run(Variables.restore_pool_genesis_file)
-            Variables.steps[4].set_status(Status.PASSED)
-        except Exception as E:
-            print(Colors.FAIL + str(E) + Colors.ENDC)
-            Variables.steps[4].set_status(Status.FAILED)
-            Variables.steps[4].set_message(str(E))
-        logger.info("Test Scenario 02 -> completed")
+            # 1. Create ledger config from genesis txn file  ---------------------------------------------------------
+            step1 = self.steps[0]
+            step1.set_name("Create Ledger")
+            pool_config = json.dumps({"genesis_txn": str(self.pool_genesis_txn_file)})
+            self.pool_handle = await perform(step1, pool.create_pool_ledger_config, self.pool_name, pool_config)
 
+            # 2. Open pool ledger -----------------------------------------------------------------------------------
+            step = 1
+            self.steps[step].set_name("Open pool ledger")
+            self.steps[step].set_message("Failed due to the Bug IS-332")
+            self.steps[step].set_status(Status.FAILED)
 
-def test(folder_path=""):
-    # Set up the report
-    begin_time = time.time()
-    Variables.test_result.setup_json_report()
+            # 3. verifying the message ------------------------------------------------------------------------
+            step = 2
+            self.steps[step].set_name("verifying the message")
+            self.steps[step].set_message("TODO after fix IS-332")
+            self.steps[step].set_status(Status.FAILED)
+        except Exception as ex:
+            print(Colors.FAIL + "Exception: " + str(ex) + Colors.ENDC)
 
-    # Precondition
-    test_precondition()
-
-    # Run test case and collect result
-    Common.run(test_scenario_02_verify_messages_on_connection)
-    Common.final_result(Variables.test_result, Variables.steps, begin_time)
+        print("Test Scenario 02 -> completed")
 
 
 if __name__ == '__main__':
-    test()
+    TestScenario02().execute_scenario()
