94a4d0831e80fe48f9ac1b96e679af89ec0c61fa
Nhan Nguyen - 6187 <nhan.trong.nguyen@logigear.com>
2017-11-28 14:14:04 +0700

    change message

--- functional_tests/test_scripts/test_runner.py
+++ functional_tests/test_scripts/test_runner.py
@@ -17,11 +17,11 @@ import multiprocessing
 import argparse
 from threading import Timer
 from indy import IndyError
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from libraries.constant import Colors, Message
 
 
 class TestRunner:
-    __SEPARATOR = "="
-    __EXECUTOR = "python3.6"
     __default_dir = os.path.dirname(os.path.abspath(__file__))
     __reporter_dir = __default_dir + "/reporter.py"
 
@@ -41,18 +41,18 @@ class TestRunner:
         test_directiory = temp if temp else TestRunner.__default_dir
 
         if not os.path.exists(test_directiory) or not os.path.isdir(test_directiory):
-            print("Directory is incorrect!")
+            print(Message.ERR_PATH_DOES_NOT_EXIST)
             return
 
-        list_test_files = TestRunner.__get_list_files(test_directiory)
+        list_test_scenarios = TestRunner.__get_list_scenarios_in_folder(test_directiory)
 
-        if not list_test_files:
-            print("Cannot find any test cases!")
+        if not list_test_scenarios:
+            print(Message.ERR_CANNOT_FIND_ANY_TEST_SCENARIOS)
             return
 
-        for test_scenario_file in list_test_files:
+        for test_scenario in list_test_scenarios:
             if self.__continue:
-                self.__execute_test_scenario(test_scenario_file)
+                self.__execute_test_scenario(test_scenario)
 
         self.__execute_reporter()
 
@@ -65,7 +65,7 @@ class TestRunner:
 
         if not time_out or time_out <= 0:
             self.run()
-            print("\033[92m" + "\nAll tests have been executed!!!\n" + "\033[0m")
+            print(Colors.OKGREEN + "\n{}\n".format(Message.INFO_ALL_TEST_HAVE_BEEN_EXECUTED) + Colors.ENDC)
         else:
             timer = Timer(float(time_out), function=self.__timeout_event)
             timer.start()
@@ -73,7 +73,7 @@ class TestRunner:
             if timer.is_alive():
                 timer.cancel()
             if self.__continue:
-                print("\033[92m" + "\nAll tests have been executed!!!\n" + "\033[0m")
+                print(Colors.OKGREEN + "\n{}\n".format(Message.INFO_ALL_TEST_HAVE_BEEN_EXECUTED) + Colors.ENDC)
             else:
                 self.__continue = False
 
@@ -82,8 +82,8 @@ class TestRunner:
         Terminate current test scenario and run it's pos-condition.
         """
         self.__continue = False
-        print("\033[91m" + "\nAborting test scenario because of time limitation...\n" + "\033[0m")
-        print("\033[95m" + "Running clean up for aborted test scenario..." + "\033[0m")
+        print(Colors.FAIL + "\n{}\n".format(Message.ERR_TEST_RUNNER_TIME_LIMITATION) + Colors.ENDC)
+        print(Colors.HEADER + "{}".format(Message.INFO_RUNNING_TEST_POS_CONDITION) + Colors.ENDC)
         if self.__test_process:
             self.__test_process.terminate()
         if self.__current_scenario:
@@ -118,41 +118,45 @@ class TestRunner:
         if reporter_path is "":
             return
         reporter_path = reporter_path if reporter_path else TestRunner.__reporter_dir
-        cmd = "{} {}".format(TestRunner.__EXECUTOR, reporter_path)
+        cmd = "{} {}".format("python3.6", reporter_path)
         subprocess.call(cmd, shell=True)
 
-    def __execute_test_scenario(self, test_scenario_file: str):
+    def __execute_test_scenario(self, test_scenario):
         """
-        Find all scenario in "test_scenario_file" and execute them.
-        :param test_scenario_file: file that contain test scenarios.
+        Execute test scenario.
+        :param test_scenario: file that contain test scenarios.
         """
-        if not test_scenario_file:
+        if not test_scenario:
             return
-        sys.path.append(os.path.dirname(os.path.abspath(test_scenario_file)))
-        test_module = importlib.import_module(os.path.basename(test_scenario_file).replace(".py", ""))
-        for name, cls in inspect.getmembers(test_module, inspect.isclass):
-            if "TestScenarioBase" in str(cls.__bases__) and self.__continue:
-                test_scenario = cls()
-                process = multiprocessing.Process(target=test_scenario.execute_scenario)
-                self.__current_scenario = test_scenario
-                self.__test_process = process
-                process.start()
-                process.join()
+        self.__current_scenario = test_scenario()
+        process = multiprocessing.Process(target=self.__current_scenario.execute_scenario)
+        self.__test_process = process
+        process.start()
+        process.join()
 
     @staticmethod
-    def __get_list_files(start_directory):
+    def __get_list_scenarios_in_folder(start_directory):
         """
-        Get all .py file in start directory and sub directories.
+        Get all test scenarios in start directory and sub directories.
         :param start_directory:
         """
         list_files = []
+
         try:
             for directory, _, _ in os.walk(start_directory):
                 list_files.extend(glob.glob(os.path.join(directory, "*.py")))
-        except Exception as e:
-            print(str(e))
+        except Exception:
+            pass
 
-        return list_files
+        list_test_scenarios = []
+        for file in list_files:
+            sys.path.append(os.path.dirname(os.path.abspath(file)))
+            test_module = importlib.import_module(os.path.basename(file).replace(".py", ""))
+            for name, cls in inspect.getmembers(test_module, inspect.isclass):
+                if "TestScenarioBase" in str(cls.__bases__):
+                    list_test_scenarios.append(cls)
+
+        return list_test_scenarios
 
 
 if __name__ == "__main__":
