9894529d6e32fe655dcc16b565da5e877fa95788
Khoi Ngo <khoi.ngo@logigear.com>
2017-12-11 12:05:29 +0700

    add test case sign request

---
+++ functional_tests/test_scripts/ledger/ledger_sign_request.py
@@ -0,0 +1,75 @@
+"""
+Created on Dec 11, 2017
+
+@author: khoi.ngo
+
+Implementing test case SignRequest with valid value.
+"""
+from indy import signus, ledger
+from libraries.test_scenario_base import TestScenarioBase
+from libraries.utils import perform, exit_if_exception
+from libraries.common import Common
+from libraries.result import Status
+import json
+from libraries.constant import Constant
+
+
+class SignRequest(TestScenarioBase):
+
+    async def execute_postcondition_steps(self):
+        pass
+
+    async def execute_test_steps(self):
+        print("SignRequest started")
+        # Prepare data to test.
+        message = json.dumps(
+            {
+                "reqId": 1496822211362017764,
+                "identifier": "GJ1SzoWzavQYfNL9XkaJdrQejfztN4XqdsiV4ct3LXKL",
+                "operation":
+                {
+                    "type": "1",
+                    "dest": "VsKV7grR1BUE29mG2Fm2kX",
+                    "verkey": "GjZWsBLgZCR18aL468JAT7w9CZRiBnpxUPPgyQxh4voa"
+                }
+            })
+        expected_signature = "65hzs4nsdQsTUqLCLy2qisbKLfwYKZSWoyh1C6CU59p5pfG3EHQXGAsjW4Qw4QdwkrvjSgQuyv8qyABcXRBznFKW"
+        # 1. Create pool
+        self.steps.add_step("Create pool Ledger")
+        result = await perform(self.steps, Common.create_and_open_pool,
+                               self.pool_name, self.pool_genesis_txn_file)
+        self.pool_handle = exit_if_exception(result)
+
+        # 2. Create and open wallet
+        self.steps.add_step("Create and open my wallet")
+        self.wallet_handle = await perform(self.steps, Common.create_and_open_wallet,
+                                           self.pool_name, self.wallet_name)
+
+        # 3. Create and store did
+        self.steps.add_step("Create DID")
+        (did, _) = await perform(self.steps, signus.create_and_store_my_did, self.wallet_handle,
+                                 json.dumps({"seed": Constant.seed_default_trustee}))
+
+        # 4. sign request
+        self.steps.add_step("sign the request")
+        json_response = await perform(self.steps, ledger.sign_request, self.wallet_handle, did, message)
+
+        # 5. verify the signature is correct.
+        self.steps.add_step("verify the signature is correct.")
+        try:
+            signed_msg = json.loads(json_response)
+            actual_signature = signed_msg['signature']
+            if actual_signature == expected_signature:
+                self.steps.get_last_step().set_status(Status.PASSED)
+            else:
+                self.steps.get_last_step().set_status(Status.FAILED)
+                self.steps.get_last_step().set_message("Failed. Expected signature is [%s] but actual signature is [%s]"
+                                                       % (expected_signature, actual_signature))
+        except Exception as E:
+            self.steps.get_last_step().set_status(Status.FAILED)
+            self.steps.get_last_step().set_message(str(E))
+        print("SignRequest completed")
+
+
+if __name__ == '__main__':
+    SignRequest().execute_scenario()
