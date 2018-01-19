47cfc89053ce239be60ddf096722b3550cf31b3d
Khoi Ngo <khoi.ngo@logigear.com>
2018-01-19 09:47:20 +0700

    aaa

---
+++ test_pytest.py
@@ -0,0 +1,12 @@
+'''
+Created on Jan 18, 2018
+
+@author: khoi.ngo
+'''
+from utilities.test_scenario_base import TestScenarioBase
+
+
+class test_khoi(TestScenarioBase):
+
+    async def test_a(self):
+        print("test_a")
